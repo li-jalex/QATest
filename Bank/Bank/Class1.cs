449d6435c9c0877969be1381c6c66a11bb6985a0
Jude <jude.sqa@gmail.com>
2012-06-29 15:49:36 -0700

    Test commit - unit test fail simulation

--- Bank/Bank/Class1.cs
+++ Bank/Bank/Class1.cs
@@ -88,8 +88,8 @@ namespace BankAccountNS
         {
             BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
-            ba.Credit(5.77);
-            ba.Debit(11.22);
+            ba.Credit(15.77);
+            ba.Debit(21.22);
             Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
 
